4dd18b41d5c8e143d9462ebd4ea2d4d3816866f1
Mateus Toledo <mateusmtoledo1@gmail.com>
2022-11-30 21:43:46 -0300

    Move home page link to h1 heading

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,25 @@
-import styles from "./styles/Header.module.css";
+import styles from './styles/Header.module.css';
 import { Link } from 'react-router-dom';
 import cartIcon from '../icons/cart.svg';
 
-function Header({numberOfCartItems}) {
-  return(
+function Header({ numberOfCartItems }) {
+  return (
     <header className={styles.header}>
-      <h1>fake store</h1>
+      <Link to="/">
+        <h1>fake store</h1>
+      </Link>
       <nav>
-        <Link to="/">
-          <p>Home</p>
-        </Link>
         <Link to="/shop">
           <p>Shop</p>
         </Link>
         <Link className={styles.goToCart} to="/cart">
           <img src={cartIcon} alt="Go to cart" />
-          <p title="Number of items in the cart" className={styles.numberOfCartItems}>{numberOfCartItems}</p>
+          <p
+            title="Number of items in the cart"
+            className={styles.numberOfCartItems}
+          >
+            {numberOfCartItems}
+          </p>
         </Link>
       </nav>
     </header>
