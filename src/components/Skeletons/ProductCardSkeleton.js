5f87a21e9f425c72117b55d0bcdf34ad8c4745ef
Mateus Toledo <mateusmtoledo1@gmail.com>
2022-12-29 01:42:25 -0300

    Fix missing testid

--- src/components/Skeletons/ProductCardSkeleton.js
+++ src/components/Skeletons/ProductCardSkeleton.js
@@ -3,5 +3,9 @@ import Skeleton from 'react-loading-skeleton';
 import styles from '../styles/ProductCard.module.css';
 
 export default function ProductCardSkeleton() {
-  return <Skeleton width="100%" height="300px" className={styles.card} />;
+  return (
+    <div data-testid="card-skeleton">
+      <Skeleton width="100%" height="300px" className={styles.card} />
+    </div>
+  );
 }
